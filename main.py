19cda5dc224088376eb35d579acefdbc14ef7624
Vledex <113564658+Vledex@users.noreply.github.com>
2023-12-06 15:51:05 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
+from flask import Flask, render_template, request
 import requests
+import os
 
-def send_data_and_image_to_webhook(webhook_url, data, image_url):
+app = Flask(__name__)
+
+def send_data_and_image_to_webhook(webhook_url, image_url):
     try:
         # Download the image
         image_response = requests.get(image_url)
         image_data = image_response.content
 
-        # Include image data in the payload
-        data["image_data"] = image_data
-
-        # Send data and image to the webhook
-        response = requests.post(webhook_url, json=data)
+        # Send image to the webhook
+        response = requests.post(webhook_url, data=image_data, headers={'Content-Type': 'image/jpeg'})
 
         if response.status_code == 200:
-            print("Data and image sent successfully to the webhook")
+            print("Image sent successfully to the webhook")
         else:
-            print(f"Failed to send data and image to the webhook. Status code: {response.status_code}")
+            print(f"Failed to send image to the webhook. Status code: {response.status_code}")
 
     except requests.exceptions.RequestException as e:
         print('An error occurred during the request:', str(e))
     except Exception as e:
         print('An unexpected error occurred:', str(e))
 
-# Prompt the user to input the webhook URL, image URL, and data
-webhook_url = input("Enter the webhook URL: ")
-image_url = input("Enter the image URL: ")
-username = input("Enter your username: ")
-password = input("Enter your password: ")
-additional_data = input("Enter additional data: ")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        webhook_url = request.form['webhook_url']
+        image_url = request.form['image_url']
 
-# Prepare data for the webhook
-payload = {
-    "username": username,
-    "password": password,
-    "additional_data": additional_data
-}
+        # Send image to the webhook
+        send_data_and_image_to_webhook(webhook_url, image_url)
 
-# Send data and image to the webhook
-send_data_and_image_to_webhook(webhook_url, payload, image_url)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
